57a93015c02da4a13ef695ebec91f8d9c5eee0b5
Sergey <sergei_smirnov@epam.com>
2019-05-18 19:29:16 +0300

    One test is added from cloned repository

--- CodingBatMavenTests/StringTwoMethods/src/test/java/StringTwoMethodsContHiTest.java
+++ CodingBatMavenTests/StringTwoMethods/src/test/java/StringTwoMethodsContHiTest.java
@@ -1,4 +1,5 @@
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 
@@ -17,17 +18,46 @@ public class StringTwoMethodsContHiTest {
             Assert.assertEquals(countHiValue,3);
 
         }
-        @Test
+        /*@Test
     public void testCountHiThreePositive() {
             StringTwoMethods countHi = new StringTwoMethods(); int countHiValue = countHi.countHi("AHIBhiChi hi") ;
             Assert.assertNotSame(countHiValue,5);
 
-        }
+        }*/
 
     @Test
     public void testCountHiFourPositive() {
         StringTwoMethods countHi = new StringTwoMethods(); int countHiValue = countHi.countHi("AHIBhiChi hi eeehi") ;
         Assert.assertNotSame(countHiValue,5);
+
+    }
+
+
+    @DataProvider(name = "CountHi")
+    public Object[][] dataProvider() {
+        return new Object[][]
+                {
+                        { "AHIBhiChi hi", 3 },
+                        { "Ahi hi", 2 },
+                        { " hi", 1 },
+                        { "Ahi hihihihhi", 5 }
+                };
+    }
+    /*@DataProvider(name = "addMethodDataProvider")
+    public Object[][] dataProvider() {
+        return new Object[][] { { 2, 5, 7 }, { 3, 7, 10 }, { 4, 5, 9 } };
+    }
+
+    @Test(dataProvider = "addMethodDataProvider")
+    public void testAddMethod(int a, int b, int result) {
+        Calculator calculator = new Calculator();
+        Assert.assertEquals(calculator.add(a, b), result);
+    }*/
+
+    @Test (dataProvider = "CountHi")
+    public void testCountHiPositive(String testString, int resultValue) {
+        StringTwoMethods countHi = new StringTwoMethods(); int countHiValue = countHi.countHi(testString) ;
+        Assert.assertNotSame(countHiValue,resultValue);
 
     }
     }
